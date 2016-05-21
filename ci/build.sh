6d331119583b1ba376c57db3490232bf0383335f
pikdum <pikdum@airmail.cc>
2016-05-21 00:32:20 -0500

    date script not working

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@ npm install -g electron-packager
 apt-get update
 apt-get install zip -y
 electron-packager nbb nbb --platform=linux,win32,darwin --arch=x64 --version=1.1.1
-DATE=$(date +%Y%m%d%H%M%S)
+DATE=`date +%Y%m%d%H%M%S`
 zip -q -r $(DATE)_nbb-linux-x64.zip nbb-linux-x64/*
 zip -q -r $(DATE)_nbb-win32-x64.zip nbb-win32-x64/*
 zip -q -r $(DATE)_nbb-darwin-x64.zip nbb-darwin-x64/*

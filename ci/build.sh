d63123e144453e3e7b3727ede9afb02180e2712e
pikdum <pikdum@airmail.cc>
2016-05-21 00:20:32 -0500

    update pipeline stuff

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-npm install -g electron-packager
-apt-get update
-apt-get install zip -y
+npm install -g electron-packager &> /dev/null
+apt-get update &> /dev/null
+apt-get install zip -y &> /dev/null
 electron-packager nbb nbb --platform=linux,win32,darwin --arch=x64 --version=1.1.1
-zip -r nbb-linux-x64.zip nbb-linux-x64/*
-zip -r nbb-win32-x64.zip nbb-win32-x64/*
-zip -r nbb-darwin-x64.zip nbb-darwin-x64/*
+DATE=$(date +%Y%m%d%H%M%S)
+zip -q -r $(DATE)_nbb-linux-x64.zip nbb-linux-x64/*
+zip -q -r $(DATE)_nbb-win32-x64.zip nbb-win32-x64/*
+zip -q -r $(DATE)_nbb-darwin-x64.zip nbb-darwin-x64/*
 mv *.zip package/

04dd1985f9219c48170891f45a27f6b99a489f91
pikdum <pikdum@airmail.cc>
2016-05-27 20:01:50 -0500

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-electron-packager /builds/pikdum/nbb --platform=linux,win32,darwin --arch=x64 --version=1.1.1
+electron-packager . nbb --platform=linux,win32,darwin --arch=x64 --version=1.1.1
 zip -q -r nbb-linux-x64.zip nbb-linux-x64/*
 zip -q -r nbb-win32-x64.zip nbb-win32-x64/*
 zip -q -r nbb-darwin-x64.zip nbb-darwin-x64/*
-ls -la
-electron-packager
